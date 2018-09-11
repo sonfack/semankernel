14da282dce1004d36e020dd673a9ec3b14a7f338
Sonfack Serge <sss.sonfack@gmail.com>
2018-09-12 03:27:44 +0800

    transform file to json format

--- queryclass.py
+++ queryclass.py
@@ -1,11 +1,15 @@
 import rdflib
+import json
 from collections import Counter
+from rdflib import Graph, plugin
+from rdflib.serializer import Serializer
 
 g = rdflib.Graph()
 g.parse("http://purl.obolibrary.org/obo/go.owl")
 
 print(len(g))
 f = open('ontologies.txt', 'a+')
+f.write('{ \n')
 for subj in g.subjects(predicate=None, object=None):
     print('SUJET = '+subj)
     subjLabel = g.label(subj, default='')
@@ -20,18 +24,18 @@ for subj in g.subjects(predicate=None, object=None):
         if len(tabPredicate) == 2:
             if object and len(object) > 0:
                 if Counter(tabPredicate[1]) == Counter('id'):
-                    f.write(tabPredicate[1] + '=' + object + ', ')
+                    f.write(tabPredicate[1] + ':' + object + ', \n' )
                 elif Counter(tabPredicate[1]) == Counter('comment'):
-                    f.write(tabPredicate[1] + '=' + g.comment(subject=subj, default='') + ', ')
+                    f.write(tabPredicate[1] + ':' + g.comment(subject=subj, default='') + ', \n')
                 else:
                     tabObject = object.split('#')
                     if len(tabObject) == 2:
-                        f.write(tabPredicate[1] + '=' + tabObject[1] + ', ')
+                        f.write(tabPredicate[1] + ':' + tabObject[1] + ', \n')
                     else:
-                        f.write(tabPredicate[1] + '=' + object + ', ')
+                        f.write(tabPredicate[1] + ':' + object + ', \n')
             else:
-                f.write(tabPredicate[1]+'='+'None, ')
-
+                f.write(tabPredicate[1]+':'+'None, \n')
+f.write('\n }')
 f.close()
 
 
